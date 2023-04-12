63e96d4c83af705619af3de72a7413b6041258e4
nicolozibra1 <nico.zibra@gmail.com>
2023-04-12 15:49:37 +0200

    import-multiple-components

---
+++ vite-hello-world/src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="hero-container">
+       <h3>{{ title }}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHero',
+        data() {
+            return{
+                title: 'Hero Here!'
+            }
+        } 
+    }
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppTitle',
+    data() {
+        return {
+            title: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
